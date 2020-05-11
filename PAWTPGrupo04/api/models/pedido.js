ee84c830e868a3dd8d1c4932fedbcb4e47af3d6e
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2020-05-11 17:20:31 +0100

    Update: Melhorada lógica dea REST API

--- PAWTPGrupo04/api/models/pedido.js
+++ PAWTPGrupo04/api/models/pedido.js
@@ -11,7 +11,7 @@ const requestSchema = new mongoose.Schema({
     grupoDeRisco: {type: Boolean, required: true},
     encaminhado_saude24: {type: Boolean, required: true},
     resultadoInicial: {type: Boolean, default: false},
-    resultadoFinal: {type: Boolean, default: false},
+    resultadoFinal: {type: Boolean},
     casoFechado: {type: Boolean}, default: false,
     deleted : {type: Boolean, default: false},
 	updated_at: { type: Date, default: Date.now },
