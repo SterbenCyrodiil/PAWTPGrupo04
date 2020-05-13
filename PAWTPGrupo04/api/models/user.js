fb0b8bfed5f5f39b7658a6609c894c65066733b4
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2020-05-13 18:28:34 +0100

    Update: Mudada a dependencia do bcrypt

--- PAWTPGrupo04/api/models/user.js
+++ PAWTPGrupo04/api/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt-nodejs')
+const bcrypt = require('bcryptjs')
 const validator = require('validator')
 
 const GenderEnum = Object.freeze({
